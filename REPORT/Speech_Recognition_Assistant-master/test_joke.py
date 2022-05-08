f4580534786bf3538384d97264845494235d349f
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-05-08 12:25:00 +0530

    Some cleanup

--- REPORT/Speech_Recognition_Assistant-master/test_joke.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import random
-
-def test_joke():
-    df = pandas.read_csv('jokes.csv')
-    jokes_list = df.values.tolist()
-    random_joke = random.choice(jokes_list)
-    return random_joke[0]
